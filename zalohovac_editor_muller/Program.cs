a33a6c530e0e5030150f869b33e5b0d8f7ef6654
Vojtěch Müller <vojtamuller2008@gmail.com>
2026-04-09 21:59:20 +0200

    Přidejte soubory projektu.

---
+++ zalohovac_editor_muller/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace zalohovac_editor_muller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
